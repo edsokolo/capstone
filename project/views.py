4a37d7d37a2c71137d0f0feeed4d9d3aaaf560f0
Eric Sokolov <eric.sokolov.55@gmail.com>
2017-09-01 14:08:28 -0700

    Added some stuff

--- project/views.py
+++ project/views.py
@@ -7,3 +7,8 @@ from .database import session
 @app.route("/", methods=["GET"])
 def start():
     return render_template("index.html")
+
+@app.route("/login", methods=["GET"])
+def login():
+    return render_template("login.html")
+

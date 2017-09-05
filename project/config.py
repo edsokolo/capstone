594664f44daae2c84fd0e2dd01d2b3fd0db8b37f
Eric Sokolov <eric.sokolov.55@gmail.com>
2017-09-05 10:25:05 -0700

    fixed secret_key

--- project/config.py
+++ project/config.py
@@ -12,4 +12,4 @@ class TestingConfig(object):
 class HerokuConfig(object):
     DATABASE_URI = "postgres://saizzkncluhzux:512e10fb5909bfa9da90766e55ceb9cc7445e430d92583e783df8d52ad13ae26@ec2-107-20-188-239.compute-1.amazonaws.com:5432/d9kvl5tlarbf2v"
     DEBUG = True
-    SECRET_KEY = os.environ.get("CAPSTONE_SECRET_KEY", os.urandom(12))
+    SECRET_KEY = os.environ.get("HEROKU_SECRET_KEY", os.urandom(12))

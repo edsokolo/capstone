244f08a8369cadab85f28e9197700faace16c762
Eric Sokolov <eric.sokolov.55@gmail.com>
2017-09-04 23:36:09 -0700

    Updated Config

--- project/__init__.py
+++ project/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-config_path = os.environ.get("CONFIG_PATH", "project.config.DevelopmentConfig")
+config_path = os.environ.get("CONFIG_PATH", "project.config.HerokuConfig")
 app.config.from_object(config_path)
 
 from . import api
--- project/config.py
+++ project/config.py
@@ -8,3 +8,8 @@ class DevelopmentConfig(object):
 class TestingConfig(object):
     DATABASE_URI = "postgresql://ericsokolov:thinkful@localhost:5432/capstone-test"
     DEBUG = True
+
+class HerokuConfig(object):
+    DATABASE_URI = "postgres://saizzkncluhzux:512e10fb5909bfa9da90766e55ceb9cc7445e430d92583e783df8d52ad13ae26@ec2-107-20-188-239.compute-1.amazonaws.com:5432/d9kvl5tlarbf2v"
+    DEBUG = True
+    SECRET_KEY = os.environ.get("CAPSTONE_SECRET_KEY", os.urandom(12))
